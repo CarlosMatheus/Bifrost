051c8417b50c8e424a837d138ae44b9445efc2bf
HikkusT <luizhenrique98@gmail.com>
2018-05-06 05:14:46 -0300

    Added support to -1 sender

--- db_manager.py
+++ db_manager.py
@@ -16,9 +16,11 @@ class DbManager:
         cls.firebase = pyrebase.initialize_app(config)
         cls.db = cls.firebase.database()
 
+
     @classmethod
     def add_to_today(cls, sender : str, receiver : str, msg : str) -> None:
         cls.db.child("Today").child(sender).push({"sender": sender, "receiver" : receiver, "text": msg})
+
 
     @classmethod
     def rm_from_today(cls, sender : str, receiver : str):
@@ -27,10 +29,12 @@ class DbManager:
             if curr_sender.key() == sender:
                 user_data = curr_sender.val()
                 for key, value in user_data.items():
-                    if user_data[key]["receiver"] == receiver:
+                    if user_data[key]["receiver"] == receiver or user_data[key]["receiver"] == "-1":
                         data = cls.db.child("Today").child(curr_sender.key()).child(key).get()
                         cls.db.child("Today").child(curr_sender.key()).child(key).remove()
-                        cls.db.child("All Time").child(sender).push(data.val())
+                        if user_data[key]["receiver"] == "-1":
+                            cls.db.child("All Time").child(sender).push(data.val())
+
 
     @classmethod
     def start_new_day(cls) -> None:
@@ -38,19 +42,22 @@ class DbManager:
         cls.db.child("All Time").set(data.val())
         cls.db.child("Today").remove()
 
+
     @classmethod
     def read_from_db(cls) -> dict:
         dict_result = {}
 
-        all_senders = cls.db.child("All Time").get()
-        for sender in all_senders.each():
-            user_data = sender.val()
-            curr_user_data = []
-            for key, value in user_data.items():
-                curr_user_data.append(value)
-            dict_result[sender.key()] = curr_user_data
+        all_senders = cls.db.child("Today").get()
+        if all_senders != None:
+            for sender in all_senders.each():
+                user_data = sender.val()
+                curr_user_data = []
+                for key, value in user_data.items():
+                    curr_user_data.append(value)
+                dict_result[sender.key()] = curr_user_data
 
         return dict_result
+
 
     @classmethod
     def check_for_msg(cls, receiver : str) -> list:
@@ -60,7 +67,7 @@ class DbManager:
         for sender in all_senders.each():
             user_data = sender.val()
             for key, value in user_data.items():
-                if user_data[key]["receiver"] == receiver:
+                if user_data[key]["receiver"] == receiver or user_data[key]["receiver"] == "-1":
                     possible_senders.append(sender.key())
 
         return possible_senders
@@ -78,11 +85,12 @@ class DbManager:
             if curr_sender.key() == sender:
                 user_data = curr_sender.val()
                 for key, value in user_data.items():
-                    if user_data[key]["receiver"] == receiver:
+                    if user_data[key]["receiver"] == receiver or user_data[key]["receiver"] == "-1":
                         text = user_data[key]["text"]
                         data = cls.db.child("Today").child(curr_sender.key()).child(key).get()
                         cls.db.child("Today").child(curr_sender.key()).child(key).remove()
-                        cls.db.child("All Time").child(sender).push(data.val())
+                        if user_data[key]["receiver"] == "-1":
+                            cls.db.child("All Time").child(sender).push(data.val())
                         break
 
         return text
